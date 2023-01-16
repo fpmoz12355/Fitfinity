7053d60fc399e8d70b7f7d2933d454c22574b3b7
Mihaela <mtovilo>
2023-01-16 22:19:00 +0100

    activity_receipts

--- app/src/main/java/ba/sum/fpmoz/fitfinity/ReceiptsActivity.java
+++ app/src/main/java/ba/sum/fpmoz/fitfinity/ReceiptsActivity.java
@@ -1,4 +1,23 @@
 package ba.sum.fpmoz.fitfinity;
 
-public class ReceiptsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class ReceiptsActivity extends AppCompatActivity{
+
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipts);
+    }
+
+
 }
